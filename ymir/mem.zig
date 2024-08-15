f267ef04e7604248fab4589c5a3db7694d78a96f
smallkirby <ssmallkirby@gmail.com>
2024-08-15 22:08:06 +0900

    add unit tests for address translation
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- ymir/mem.zig
+++ ymir/mem.zig
@@ -42,11 +42,30 @@ pub fn virt2phys(addr: Virt) Phys {
 /// This function just use simple calculation and does not walk page tables.
 /// To do page table walk, use arch-specific functions.
 pub fn phys2virt(addr: Phys) Virt {
-    return if (addr < ymir.kernel_base) b: {
-        // Direct mapping region.
-        break :b addr + ymir.direct_map_base;
-    } else b: {
-        // Kernel image mapping region.
-        break :b addr + ymir.kernel_base;
-    };
+    return addr + ymir.direct_map_base;
+}
+
+// ========================================
+
+const testing = std.testing;
+
+test {
+    testing.refAllDeclsRecursive(@This());
+}
+
+test "address translation" {
+    const direct_map_base = ymir.direct_map_base;
+    const kernel_base = ymir.kernel_base;
+
+    // virt -> phys
+    try testing.expectEqual(0x0, virt2phys(direct_map_base));
+    try testing.expectEqual(0x100, virt2phys(direct_map_base + 0x100));
+    try testing.expectEqual(ymir.arch.page_size * 0x100, virt2phys(direct_map_base + ymir.arch.page_size * 0x100));
+    try testing.expectEqual(kernel_base - direct_map_base - 1, virt2phys(kernel_base - 1));
+    try testing.expectEqual(0, virt2phys(kernel_base));
+    try testing.expectEqual(0x100000, virt2phys(kernel_base + 0x100000));
+
+    // phys -> virt
+    try testing.expectEqual(direct_map_base, phys2virt(0x0));
+    try testing.expectEqual(direct_map_base + 0x100, phys2virt(0x100));
 }
