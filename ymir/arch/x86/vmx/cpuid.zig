3180f39c71dc027eb8d679da19b7a92f6d2904e4
smallkirby <ssmallkirby@gmail.com>
2024-09-18 17:13:34 +0900

    use non-exhaustive enum for vm CPUID
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- ymir/arch/x86/vmx/cpuid.zig
+++ ymir/arch/x86/vmx/cpuid.zig
@@ -44,7 +44,7 @@ const ext_feature0_ebx = cpuid.ExtFeatureEbx0{
 pub fn handleCpuidExit(vcpu: *Vcpu) VmxError!void {
     const regs = &vcpu.guest_regs;
 
-    if (std.meta.intToEnum(Leaf, regs.rax)) |leaf| switch (leaf) {
+    switch (Leaf.from(regs.rax)) {
         .maximum_input => {
             regs.rax = 0x20; // Maximum input value for basic CPUID.
             regs.rbx = 0x72_69_6D_59; // Ymir
@@ -111,13 +111,10 @@ pub fn handleCpuidExit(vcpu: *Vcpu) VmxError!void {
                 },
             }
         },
-        else => {
+        _ => {
             log.warn("Unhandled CPUID: Leaf=0x{X:0>8}, Sub=0x{X:0>8}", .{ regs.rax, regs.rcx });
             invalid(vcpu);
         },
-    } else |_| {
-        log.warn("Unimplemented CPUID: Leaf=0x{X:0>8}, Sub=0x{X:0>8}", .{ regs.rax, regs.rcx });
-        invalid(vcpu);
     }
 }
 
@@ -140,43 +137,15 @@ const Leaf = enum(u32) {
     /// Structured extended feature enumeration.
     /// Output depends on the value of ECX.
     ext_feature = 0x7,
-    /// Extended topology enumeration.
-    ext_topology = 0xB,
     /// Processor extended state enumeration.
     /// Output depends on the ECX input value.
     ext_enumuration = 0xD,
-    /// Intel Resouce Director Technology monitoring enumeration when ECX = 0,
-    /// L3 Cache RDT monitoring capability enumeration when ECX = 1.
-    rdt_l3cache = 0xF,
-    /// RDT allocation enumeration when ECX = 0,
-    /// L3 cache allocation technology enumeration when ECX = 1.
-    /// L2 cache allocation technology enumeration when ECX = 2.
-    /// Memory bandwidth allocation enumeration when ECX = 3.
-    cache_allocation = 0x10,
-    /// Intel SGX capability enumeration when ECX = 0.
-    /// SGX attributes enumeration when ECX = 1.
-    /// SGX EPC enumeration when ECX = 2.
-    sgx = 0x12,
-    /// V2 extended topology enumeration.
-    v2_ext_topology = 0x1F,
     /// Maximum input value for extended function CPUID information.
     extended_function = 0x80000000,
     /// EAX: Extended processor signature and feature bits.
     extended_processor_signature = 0x80000001,
-    /// Processor brand string.
-    brand1 = 0x80000002,
-    /// Processor brand string continued.
-    brand2 = 0x80000003,
-    /// Processor brand string continued.
-    brand3 = 0x80000004,
-    /// Reserved.
-    reserved = 0x80000005,
-    /// Cache line size.
-    cacheline = 0x80000006,
-    /// Envariant TSC available.
-    invariant_tsc = 0x80000007,
-    /// Linear/Physical address size.
-    address_size = 0x80000008,
+    /// Unimplemented
+    _,
 
     pub fn from(rax: u64) Leaf {
         return @enumFromInt(rax);
