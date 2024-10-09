e74a0496a82026b1c764af3f6937cc630d50c238
smallkirby <ssmallkirby@gmail.com>
2024-10-09 19:39:45 +0900

    add tests for ymir.bits
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- ymir/bits.zig
+++ ymir/bits.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 /// Set the integer where only the nth bit is set.
 pub fn setbit(T: type, nth: anytype) T {
     const val = switch (@typeInfo(@TypeOf(nth))) {
@@ -21,6 +23,32 @@ pub inline fn isset(val: anytype, nth: anytype) bool {
 /// Concatnate two values and returns new value with twice the bit width.
 pub inline fn concat(T: type, a: anytype, b: @TypeOf(a)) T {
     const U = @TypeOf(a);
-    if (@bitSizeOf(T) != @bitSizeOf(U) * 2) @compileError("concat: invalid type");
-    return (@as(T, a) << @bitSizeOf(U)) | @as(T, b);
+    const width_T = @typeInfo(T).Int.bits;
+    const width_U = switch (@typeInfo(U)) {
+        .Int => |t| t.bits,
+        .ComptimeInt => width_T / 2,
+        else => @compileError("concat: invalid type"),
+    };
+    if (width_T != width_U * 2) @compileError("concat: invalid type");
+    return (@as(T, a) << width_U) | @as(T, b);
+}
+
+const testing = std.testing;
+
+test "setbit" {
+    try testing.expectEqual(0b0000_0001, setbit(u8, 0));
+    try testing.expectEqual(0b0001_0000, setbit(u8, 4));
+    try testing.expectEqual(0b1000_0000, setbit(u8, 7));
+}
+
+test "isset" {
+    try testing.expectEqual(true, isset(0b10, 1));
+    try testing.expectEqual(false, isset(0b10, 0));
+    try testing.expectEqual(true, isset(0b1000_0000, 7));
+    try testing.expectEqual(false, isset(0b1000_0000, 99));
+}
+
+test "concat" {
+    try testing.expectEqual(0b10, concat(u2, @as(u1, 1), @as(u1, 0)));
+    try testing.expectEqual(0x1234, concat(u16, 0x12, 0x34));
 }
